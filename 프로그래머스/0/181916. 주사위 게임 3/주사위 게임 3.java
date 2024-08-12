efcc1baf1fe9a55d5a9560034c72424bafe54c1a
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-08-12 16:05:03 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.66 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+	public int solution(int a, int b, int c, int d) {
+		int answer = 0;
+
+		int[] arr = { a, b, c, d };
+		Arrays.sort(arr);
+
+		if (arr[0] == arr[3]) {
+			answer = 1111 * arr[0];
+		} else if (arr[0] == arr[2] || arr[1] == arr[3]) {
+			answer = (int) (arr[0] == arr[2] ? Math.pow(10 * arr[0] + arr[3], 2) : Math.pow(10 * arr[3] + arr[0], 2));
+		} else if (arr[0] == arr[1] && arr[2] == arr[3]) {
+			answer = (arr[0] + arr[2]) * Math.abs(arr[0] - arr[2]);
+		} else if (arr[0] == arr[1] | arr[1] == arr[2] || arr[2] == arr[3]) {
+			answer = (arr[0] == arr[1] ? arr[2] * arr[3] : arr[1] == arr[2] ? arr[0] * arr[3] : arr[0] * arr[1]);
+		} else {
+			answer = arr[0];
+		}
+
+		return answer;
+	}
+}

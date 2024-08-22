c592cda10845b98e00a30955a02750610bed5ca0
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-08-22 16:58:03 +0900

    [Bronze I] Title: 단어 공부, Time: 240 ms, Memory: 21084 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,30 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-        int[] arr = new int[26];
-        
-        for(int i=0; i<str.length(); i++){
-            if('A'<=str.charAt(i) && str.charAt(i)<='Z'){
-                arr[str.charAt(i)-'A']++;
-            } else{
-                arr[str.charAt(i)-'a']++;
-            }
-        }
-        
-        int max = -1;
-        char ch = 0;
-        
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                ch = (char)(i+65);
-            }else if(arr[i] == max){
-                ch = '?';
-            }
-        }
-        System.out.print(ch);
+
+	public static int[] getCount(String str) {
+		int[] count = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+			count[str.charAt(i) - 'A']++;
+		}
+
+		return count;
 	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+		String string = bReader.readLine().toUpperCase();
+		int[] cnt = getCount(string);
+		int maxCnt = -1;
+		char maxAlphabet = '?';
+
+		for (int i = 0; i < cnt.length; i++) {
+			if (cnt[i] > maxCnt) {
+				maxCnt = cnt[i];
+				maxAlphabet = (char) ('A' + i);
+			} else if (cnt[i] == maxCnt) {
+				maxAlphabet = '?';
+			}
+		}
+
+		System.out.println(maxAlphabet);
+
+	}
+
 }

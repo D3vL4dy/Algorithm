90bb7184cc7d2d404d200e27dfa537b97cf274ca
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-08-26 23:40:36 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+		String s = "\\    /\\\n" +
+				   " )  ( ')\n" +
+				   "(  /  )\n" + 
+				   " \\(__)|\n";
+
+        System.out.println(s);
+    }
+}

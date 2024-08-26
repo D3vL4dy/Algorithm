f6ed160b81bd109ad9f9781986bad3bb44a34817
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-08-26 22:59:16 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		int[] arr = new int[26];
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = -1;
-		}
-        
-        for(int i=0; i<str.length(); i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[26];
+        Arrays.fill(arr, -1);
+
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(arr[ch-'a'] == -1){
-                arr[ch-'a'] = i;
+            if (arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
             }
         }
-	    for(int val:arr){
-            System.out.print(val+" ");
+
+        for(int val : arr) {
+            System.out.print(val + " ");
         }
-	}
+
+    }
 }

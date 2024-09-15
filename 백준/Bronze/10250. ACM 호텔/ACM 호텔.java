964f29d371ca10cd389ed92f1a75ef3e791889e9
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-09-16 00:44:32 +0900

    [Bronze III] Title: ACM 호텔, Time: 136 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int floor = ((N - 1) % H) + 1;
+            int distance = (N - 1) / H + 1;
+            System.out.printf("%d%02d\n", floor, distance);
+        }
+    }
+}

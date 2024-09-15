1ee43caff9c0f4382124a3d2d7ed67252023a74a
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-09-15 23:42:00 +0900

    [Bronze I] Title: 진법 변환 2, Time: 104 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -12,19 +12,15 @@ public class Main {
 
         // 1. N을 B로 나눈 나머지를 구하고, B로 나눈다.
         // 2. 이때 가장 마지막 나머지부터 가장 앞 자릿값이 된다.
-        String ans = "";
+        StringBuilder sb = new StringBuilder();
         while (N > 0) {
             int D = N % B;
             if (D < 10)
-                ans += D;
+                sb.append(D);
             else
-                ans += (char) (D - 10 + 'A'); // 10부터 시작하는 숫자를 문자 'A'부터 대응
+                sb.append((char) (D - 10 + 'A')); // 10부터 시작하는 숫자를 문자 'A'부터 대응
             N = N / B;
         }
-
-        for (int i = ans.length() - 1; i >= 0; i--) {
-            System.out.print(ans.charAt(i));
-        }
-
+        System.out.println(sb.reverse().toString());
     }
 }

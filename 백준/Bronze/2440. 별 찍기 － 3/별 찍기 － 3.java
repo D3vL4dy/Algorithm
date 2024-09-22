12ed2432eaa2ec445be2abd15a1d1013256bdf63
Jeongin Kang <97331114+D3vL4dy@users.noreply.github.com>
2024-09-22 22:27:26 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            for (int j = T - i; j > 0; j--) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
